98f73ff5b65c56573d74d618f5cc807483742dfe
vishal <vishal.agg.gupta007@gmail.com>
2021-05-17 14:53:40 +0530

    added repsonsiveness to homepage

---
+++ src/Components/Homepage/Circle/Circle.js
@@ -0,0 +1,22 @@
+import "./Circle.css";
+import React from "react";
+
+function Circle({ icon, count, label, colorClass }) {
+  return (
+    <div
+      className={`homepage__circle ${
+        colorClass === true ? "homepage__circle__active" : null
+      }  `}
+    >
+      <div className="homepage__circle__content">
+        <div>
+          <img src={icon} />
+        </div>
+        <div className="homepage__circle__count">{count}</div>
+        <div className="homepage__circle__label">{label}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Circle;
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,11 +1,42 @@
 import "./Homepage.css";
 import React from "react";
-import Navbrand from "./navbrand.png";
-import BGImage from "./Rectangle.png";
-import Overlay from "./Overlay.png";
+import Navbrand from "./images/navbrand.png";
+import BGImage from "./images/Rectangle.png";
+import Overlay from "./images/Overlay.png";
 import SearchIcon from "@material-ui/icons/Search";
+import Circle from "./Circle/Circle.js";
+import Heart from "./images/Vector.svg";
+import Calender from "./images/Calendar.svg";
+import Cart from "./images/Cart.svg";
 
 function Homepage() {
+  const circle_details = [
+    {
+      icon: Heart,
+      count: 0,
+      label: "label",
+      colorClass: true,
+    },
+    {
+      icon: Calender,
+      count: 0,
+      label: "label",
+      colorClass: false,
+    },
+    {
+      icon: Calender,
+      count: 0,
+      label: "label",
+      colorClass: false,
+    },
+    {
+      icon: Calender,
+      count: 0,
+      label: "label",
+      colorClass: false,
+    },
+  ];
+
   return (
     <div className="customNavbar">
       <img className="bgimage" src={BGImage} />
@@ -15,6 +46,11 @@ function Homepage() {
         <a class="navbar-brand" href="#">
           <img src={Navbrand} />
         </a>
+        <div className="navbar__reponsive__button">
+          <SearchIcon className="navbar__search" />
+          <img src={Cart} />
+        </div>
+
         <button
           class="navbar-toggler"
           type="button"
@@ -30,6 +66,11 @@ function Homepage() {
         <div class="collapse navbar-collapse " id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
+              <a class="nav-link navbar__search" href="#">
+                <SearchIcon />
+              </a>
+            </li>
+            <li class="nav-item active">
               <a class="nav-link" href="#">
                 Search
               </a>
@@ -44,6 +85,9 @@ function Homepage() {
                 Account
               </a>
             </li>
+            <li class="nav-item active navbar__cart">
+              <img src={Cart} />
+            </li>
           </ul>
         </div>
       </nav>
@@ -52,11 +96,24 @@ function Homepage() {
           <b>Cari Cari</b>
         </h1>
         <p class="para">
-          Live from their sofa to yours. Get closer to your favorite
+          Live from their sofa to yours. Get closer to your favorite artists,
+          and never miss out.
         </p>
-        <p class="para1">artists, and never miss out.</p>
       </div>
-    </div>
+
+        <div className="homepage__circle__maindiv">
+          {circle_details.map((e) => {
+            return (
+              <Circle
+                icon={e.icon}
+                count={e.count}
+                label={e.label}
+                colorClass={e.colorClass}
+              />
+            );
+          })}
+        </div>
+      </div>
   );
 }
 
